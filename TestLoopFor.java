e1766f0ac84cc928ea3cabbdf4528f195ff610c2
อาฬสา <149029447+arasa22@users.noreply.github.com>
2024-07-31 15:42:59 +0700

    Create TestLoopFor.java

---
+++ TestLoopFor.java
@@ -0,0 +1,9 @@
+public class TestLoopFor {
+    public static void main(String[] args) {
+        int number = 2; 
+        for (int i = 1; i <= 12; i++) {
+            int result = number * i;
+            System.out.println(number + " x " + i + " = " + result);
+        }
+    }
+}
