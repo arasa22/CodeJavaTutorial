e68b1eb636eee731c1413feecc7f7afb6fe26147
อาฬสา <149029447+arasa22@users.noreply.github.com>
2024-08-01 14:21:15 +0700

    Create ExNo3.java

---
+++ ExNo3.java
@@ -0,0 +1,9 @@
+public class ExNo3 {
+    public static void main(String[] args) {
+        int num = 8;
+        for (int i = 1; i <= 12; i++) {
+            int result = num * i;
+            System.out.println(num + " x " + i + " = " + result);
+        }
+    }
+}
