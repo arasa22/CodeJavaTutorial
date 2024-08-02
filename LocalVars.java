ac0b24691af842de2d93718ccd65e577f6db5163
อาฬสา <149029447+arasa22@users.noreply.github.com>
2024-08-02 15:08:35 +0700

    Create LocalVars.java

---
+++ LocalVars.java
@@ -0,0 +1,15 @@
+public class LocalVars {
+    public static void main(String[] args) {
+        phuket();
+        rayoung();
+        phuket();
+    }
+    public static void phuket() {
+        int brids = 5000;
+        System.out.println("In phuket there are " + brids + " brids.");
+    }
+    public static void rayoung() {
+        int brids = 3500;
+        System.out.println("In rayoung there are " + brids + " brids.");
+    }
+}
