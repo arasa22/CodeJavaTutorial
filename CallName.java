b9cec33ce9c42bd0706b0dceb5f1e8e4ceac779c
อาฬสา <149029447+arasa22@users.noreply.github.com>
2024-08-02 10:09:02 +0700

    Create CallName.java

---
+++ CallName.java
@@ -0,0 +1,11 @@
+public class CallName {
+    public static void main(String[] args) {
+        System.out.println("The program generates a method that asks you to type your name 10 times.");
+        for (int i = 1; i <= 10; i++){
+            name();
+        }
+    }
+    public static void name() {
+        System.out.println("Arasa Laopanna.");
+    }
+}
